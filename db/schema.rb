6d77154eceb275f80faa188616275fd3a88cd4d0
Mario Perez <mapreal19@gmail.com>
2015-10-15 15:29:12 +0200

    Copied migrations from blog engine.
    `rake blorgh:install:migrations`

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012102900) do
+ActiveRecord::Schema.define(version: 20151015130246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blorgh_articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
--- engines/blorgh/config/routes.rb
+++ engines/blorgh/config/routes.rb
@@ -1,3 +1,4 @@
 Blorgh::Engine.routes.draw do
+  root 'articles#index'
   resources :articles
 end
